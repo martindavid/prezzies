ddffc11fbb65314b38e76b074e6fff1bcffed6fb
Martin David Valentino <martin.david.siagian@gmail.com>
2016-12-10 22:00:31 +1100

    home page version 3

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,26 @@
 $(function() {
-  $('.first-category').click(function(obj) {
-    var a = obj;
-    alert(a);
+ 
+
+  $('#surprise-me-button').click(function(){
+    var numOfPeople = $('#person-num-select').val();
+    var budget = $('#budget-select').val();
+    var url = "http://api.diffbot.com/v3/AmazonGifts?token=28b8a52cd5034bb1446ab8cdc7f318eb&url=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fgift-finder%3FageGroup%3Dkid8";
+    // $.get(url, function(data) {
+    //   var items = data.objects[0].items;
+    //   alext()
+    // });
+    $('#loader').show();
+    setTimeout(function(){ 
+      $('#recommendation-part').show();
+    $('#loader').hide(); 
+    }, 3000);
+    
   });
 
   $(document).ready(function() {
-    $('#second-category-container').hide();
-    $('#finish-button').hide();
-    $('#categories-span').hide();
-    $('#budget-span').hide();
+    $('#recommendation-part').hide();
+    $('#loader').hide();
   });
+
+
 });
