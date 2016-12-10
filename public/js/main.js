4a4fe056a0fb643a6350b9ef2372139f509d9833
Martin David Valentino <martin.david.siagian@gmail.com>
2016-12-10 18:14:47 +1100

    home page version 2

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,13 @@
 $(function() {
-  // Your custom JavaScript goes here
+  $('.first-category').click(function(obj) {
+    var a = obj;
+    alert(a);
+  });
+
+  $(document).ready(function() {
+    $('#second-category-container').hide();
+    $('#finish-button').hide();
+    $('#categories-span').hide();
+    $('#budget-span').hide();
+  });
 });
